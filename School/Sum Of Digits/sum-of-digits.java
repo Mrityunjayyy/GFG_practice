44f69efb6c9ab5333c3731a66d99309803099b77
Mrityunjay <122476969+Mrityunjayyy@users.noreply.github.com>
2023-06-15 16:06:26 +0530

    Added Solution - GfG to GitHub

---
+++ School/Sum Of Digits/sum-of-digits.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.sumOfDigits(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static int sumOfDigits(int N) {
+      return sum(N)  ;
+    }
+    
+    
+    public static int sum(int n)
+    {
+        
+        int last = n%10;
+        
+        if( n < 10)
+        return n;
+        
+        else
+        { 
+            return last + sum(n/10);
+        }
+    }
+        
+        
+}
+       
