a76c79a9f91278c53742171cd75cef5543c4077b
Mrityunjay <122476969+Mrityunjayyy@users.noreply.github.com>
2023-07-15 21:34:17 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Sort an array of 0s, 1s and 2s/sort-an-array-of-0s-1s-and-2s.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+//Initial template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function template for Java
+
+class Solution
+{
+    
+    //dutch flag algorithm (netherlands)
+    //red - white - blue
+    public static void sort012(int a[], int n)
+    {
+        
+        
+       int[] nums = a; //galti se nums ka code likh dia
+       int low = 0; 
+       int high  = n -1 ;
+       int mid = 0;
+       
+       while(mid <= high)
+       {
+           switch(nums[mid])
+           { 
+               case 0:
+                   { 
+                       int temp = nums[low];
+                       nums[low] = nums[mid];
+                       nums[mid] = temp;
+                       low++;
+                       mid++;
+                       break;
+                   }
+                   
+                case 1:
+                    {
+                        mid++;
+                        break;
+                    }
+                    
+                case 2:
+                    { 
+                        int temp = nums[high];
+                        nums[high] = nums[mid];
+                        nums[mid] = temp;
+                        high--;
+                        break;
+                    }
+           }
+       }
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+    
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+        while(t-->0){
+            int n = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for(int i=0; i<n; i++){
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            Solution ob=new Solution();
+            ob.sort012(arr, n);
+            StringBuffer str = new StringBuffer();
+            for(int i=0; i<n; i++){
+                str.append(arr[i]+" ");
+            }
+            System.out.println(str);
+        }
+    }
+}
+
+
+// } Driver Code Ends
