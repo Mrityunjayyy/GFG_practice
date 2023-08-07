cc2dd31ba6c256991e6385d9666a74f7d8c88321
Mrityunjay <122476969+Mrityunjayyy@users.noreply.github.com>
2023-08-07 12:30:11 +0530

    Updated Solution - GfG to GitHub

--- Hard/Solve the Sudoku/solve-the-sudoku.java
+++ Hard/Solve the Sudoku/solve-the-sudoku.java
@@ -47,71 +47,67 @@ class Solution
     //Function to find a solved Sudoku. 
     static boolean SolveSudoku(int grid[][])
     {
-        for(int i = 0 ; i < grid.length ; i++)
-        { 
-            for(int j = 0 ; j < grid[0].length ; j++)
-            { 
-                if(grid[i][j] == 0)
-                { 
-                    for(int x = 1  ; x <= 9 ;x++)
-                    { 
-                        if(isValid(grid , i , j , x))
-                        { 
-                            grid[i][j] =  x;
+        return solve(grid);
+    }
+    
+    static boolean solve(int[][] a )
+    {
+        for(int i = 0 ; i < a.length ; i++)
+        {
+            for(int j = 0 ; j < a[0].length ; j++)
+            {
+                if(a[i][j] == 0)
+                {
+                    for(int m = 1 ; m <= 9 ; m++)
+                    {
+                        if(isValid(a, i , j , m))
+                        {
+                            a[i][j] = m;
                             
-                            if(SolveSudoku(grid) == true)
+                            if(solve(a) == true)
                             return true;
                             
-                            else
-                            grid[i][j] = 0;
+                            else 
+                            a[i][j] = 0;
                         }
                     }
-                    
-                    // 1-9 kuch bi nahi rakh paya pehla number galat rakh dia tha 
                     return false;
                 }
+               
             }
         }
         
-        //No empty block found full maze solved;
         return true;
     }
     
-
     
     
-    public static boolean isValid(int[][] board , int row , int column , int x)
-    {  
-        // 9 row check , 9 column check and 3*3 inner matrix check
+    static boolean isValid(int[][] a , int row , int col , int m)
+    {
         for(int i = 0 ; i < 9 ; i++)
-        { 
+        {
+            if (a[row][i] == m) return false;
+        
+            if (a[i][col] == m) return false;
             
-            if(board[row][i] == x) return false;
+            int nrow = 3 * (row/3) + (i/3);
+            int ncol = 3 * (col/3) + (i%3);
             
-            if(board[i][column] == x) return false;
-            
-            int m = 3* (row/3) + i/3;
-            int n = 3 * (column/3) + i%3;
-            
-            if(board[m][n] == x) return false;
+            if(a[nrow][ncol] == m) return false;
         }
         
-        
         return true;
-        
     }
-    
-
     
     //Function to print grids of the Sudoku.
     static void printGrid (int grid[][])
     {
-       for(int a[] : grid)
-       { 
-           for(int b : a)
-           {
-               System.out.print(b + " ");
-           }
-       }
+        for(int[] a : grid)
+        {
+            for(int b : a)
+            {
+                System.out.print(b  + " ");
+            }
+        }
     }
 }
