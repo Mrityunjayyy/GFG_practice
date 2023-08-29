826a6aaf8566fb46781e932215664f1d25dcc312
Mrityunjay <122476969+Mrityunjayyy@users.noreply.github.com>
2023-08-29 08:40:41 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Delete nodes having greater value on right/delete-nodes-having-greater-value-on-right.java
@@ -0,0 +1,134 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+
+
+public class LinkedList
+{
+    
+ 
+
+    /* Function to print linked list */
+   public static void print(Node root)
+    {
+        Node temp = root;
+        while(temp!=null)
+        {
+            System.out.print(temp.data + " ");
+            temp=temp.next;
+        }
+    }
+
+
+    /* Driver program to test above functions */
+    public static void main(String args[])
+    {
+
+        /* Constructed Linked List is 1.2.3.4.5.6.
+         7.8.8.9.null */
+        int value;
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0 && sc.hasNextInt() )
+        {
+            int n = sc.nextInt();
+            
+            //int n=Integer.parseInt(br.readLine());
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node tail = head;
+
+            for (int i = 1; i < n && sc.hasNextInt() ; i++)
+            {
+                int a = sc.nextInt();
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+
+
+            Node result = new Solution().compute(head);
+            print(result);
+            System.out.println();
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Node {
+   int data;
+   Node next;
+
+  Node(int data) {
+      this.data = data;
+  }
+}
+*/
+class Solution
+{
+    Node compute(Node head)
+    {
+       head = reverse(head);
+       Node temp = head;
+      // System.out.println("after reverse head is :: " + temp.data+ "::");
+       int max = head.data;
+     //  System.out.println("head ka data:: " + max + " :: ");
+       
+       Node prev = head;
+       Node curr = head.next;
+       
+    
+       
+       while(curr != null)
+       {
+           if(curr.data >= max)
+           {
+                max = curr.data;
+                prev = curr;
+                curr = curr.next;
+           }
+           else 
+           {
+               prev.next = prev.next.next;
+               curr = prev.next;
+           }
+       }
+       
+       head = reverse(temp);
+       return head;
+    }
+    
+    
+    public Node reverse(Node head)
+    {
+        Node current = head;
+        Node next  = null;
+        Node prev = null;
+        
+        while(current != null)
+        {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+        
+        return head;
+    }
+}
+  
